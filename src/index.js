a8da81a8ba1ebd1cd9efff158012774f08f8226b
Navis Ayara <navis.ochieng@student.moringaschool.com>
2025-06-22 20:10:48 +0300

    added active hints for selected posts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const postListContainer = document.getElementById("post-list");
-const postDetailsContainer = document.getElementById("post-details")
+const postDetailsContainer = document.getElementById("post-details");
 
 function openPostDetails(postId) {
     fetch(`http://localhost:3000/posts/${postId}`)
@@ -16,10 +16,21 @@ function openPostDetails(postId) {
                 </p>
               </div>
             </div>
-            `
-            postDetailsContainer.innerHTML = postCard
-        })
-}
+            `;
+            postDetailsContainer.innerHTML = postCard;
+            for (const listItem of postListContainer.children) {
+                // Clear selected state from all items
+                listItem.classList.remove("bg-black", "text-white");
+                listItem.classList.add("bg-white", "text-black");
+                        
+                // Set selected state on the clicked item
+                if (listItem.id == postId) {
+                    listItem.classList.remove("bg-white", "text-black");
+                    listItem.classList.add("bg-black", "text-white");
+                }
+            }
+        });
+};
 
 function displayPosts() {
     fetch("http://localhost:3000/posts")
@@ -27,7 +38,7 @@ function displayPosts() {
         .then(posts => {
             posts.forEach(post => {
                 const postCard = `
-                <li onclick="openPostDetails(${post.id})" class="text-left w-full p-2 border border-black hover:bg-black hover:text-white transition-colors cursor-pointer">
+                <li onclick="openPostDetails(${post.id})" id="${post.id}"class="text-left w-full p-2 border border-black hover:bg-black hover:text-white transition-colors cursor-pointer">
                 <h2 class="block text-md font-semibold">${post.title}</h2>
                 <p className="text-xs text-neutral-700">${post.timestamp}</p>
                 </li>
